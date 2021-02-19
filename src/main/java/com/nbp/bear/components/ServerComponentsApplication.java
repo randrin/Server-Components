7ad38a5fd3402136ad1930347aad57bb99702e2e
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2021-02-19 14:40:50 +0100

    Get user by token service (update)

--- src/main/java/com/nbp/bear/components/ServerComponentsApplication.java
+++ src/main/java/com/nbp/bear/components/ServerComponentsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
